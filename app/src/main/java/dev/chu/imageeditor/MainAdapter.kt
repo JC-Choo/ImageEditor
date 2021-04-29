66a74096d08aeba74643c1ecc1f86b3b35756a6c
JC-Choo <jicheolchoo@gmail.com>
2021-04-29 20:12:31 +0900

    Add main
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

---
+++ app/src/main/java/dev/chu/imageeditor/MainAdapter.kt
@@ -0,0 +1,30 @@
+package dev.chu.imageeditor
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import dev.chu.imageeditor.databinding.ItemViewBinding
+
+class MainAdapter(
+    private val callback: (Media) -> Unit
+): RecyclerView.Adapter<MainViewHolder>() {
+
+    private val items = mutableListOf<Media>()
+
+    fun setNewItems(newItems: List<Media>) {
+        items.clear()
+        items.addAll(newItems)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val binding = ItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MainViewHolder(binding, callback)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.count()
+}
---
+++ app/src/main/java/dev/chu/imageeditor/MainViewHolder.kt
@@ -0,0 +1,19 @@
+package dev.chu.imageeditor
+
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import dev.chu.imageeditor.databinding.ItemViewBinding
+
+class MainViewHolder(
+    private val binding: ItemViewBinding,
+    private val callback: (Media) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: Media) {
+        with(binding.imageView) {
+            Glide.with(context).load(item.getUri()).into(this)
+            click { callback(item) }
+        }
+        binding.executePendingBindings()
+    }
+}
