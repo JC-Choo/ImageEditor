ead7ff2f62f17f7bb6a31f6e107a3a0e7f8f4aec
JC-Choo <jicheolchoo@gmail.com>
2021-05-17 22:43:10 +0900

    Modify code
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

--- editor/src/main/java/dev/chu/editor/ui/ToolbarViewModel.kt
+++ editor/src/main/java/dev/chu/editor/ui/ToolbarViewModel.kt
@@ -1,7 +1,84 @@
 package dev.chu.editor.ui
 
+import android.content.Context
+import android.view.View
+import androidx.annotation.DrawableRes
 import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import dev.chu.core.util.ext.getColorById
+import dev.chu.custom_gallery.BR
+import dev.chu.custom_gallery.R
 import javax.inject.Inject
 
-class ToolbarViewModel @Inject constructor() : BaseObservable() {
+class ToolbarViewModel @Inject constructor(
+    context: Context
+) : BaseObservable() {
+
+    // left image
+    @DrawableRes
+    var leftImageDrawable: Int = 0
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.leftImageDrawable)
+        }
+
+    var title: String = context.getString(R.string.all_picture)
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.title)
+        }
+
+    // right image
+    var rightImageDrawable: Int = 0
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.rightImageDrawable)
+        }
+
+    var isRightVisible: Boolean = false
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.rightVisible)
+        }
+
+    // screen color
+    var backgroundColor: Int = context.getColorById(R.color.white)
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.backgroundColor)
+        }
+
+    var titleColor: Int = 0
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.titleColor)
+        }
+
+    // click event
+    var onClickLeft: View.OnClickListener? = null
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.onClickLeft)
+        }
+
+    var onClickTitle: View.OnClickListener? = null
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.onClickTitle)
+        }
+
+    var onClickRight: View.OnClickListener? = null
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.onClickRight)
+        }
 }
---
+++ editor/src/main/java/dev/chu/editor/util/ItemDecoration.kt
@@ -0,0 +1,10 @@
+package dev.chu.editor.util
+
+import android.graphics.Canvas
+import androidx.recyclerview.widget.RecyclerView
+
+class HorizontalDecoration(): RecyclerView.ItemDecoration() {
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDraw(c, parent, state)
+    }
+}
