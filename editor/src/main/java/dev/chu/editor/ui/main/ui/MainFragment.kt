343d632bdf453e0a7ede356411b762479a560bc2
JC-Choo <jicheolchoo@gmail.com>
2021-06-03 22:30:45 +0900

    Modify ui and data
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

--- editor/src/main/java/dev/chu/editor/ui/main/ui/MainFragment.kt
+++ editor/src/main/java/dev/chu/editor/ui/main/ui/MainFragment.kt
@@ -6,14 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dagger.android.support.DaggerFragment
 import dev.chu.editor.R
 import dev.chu.editor.databinding.FragmentMainBinding
+import dev.chu.editor.ui.main.data.EditTypeItem
 
 class MainFragment: DaggerFragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding: FragmentMainBinding get() = _binding!!
+
+    private val adapter by lazy {
+        MainAdapter()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,10 +42,30 @@ class MainFragment: DaggerFragment() {
                     requireActivity().finish()
                 }
             })
+
+        binding.list.layoutManager = LinearLayoutManager(
+            requireContext(),
+            RecyclerView.HORIZONTAL,
+            false
+        )
+        binding.list.adapter = adapter
+        adapter.setNewItems(getEditTypeList())
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getEditTypeList(): ArrayList<EditTypeItem> {
+        val list = ArrayList<EditTypeItem>()
+        list.add(EditTypeItem(R.drawable.arrow_back_000000, getString(R.string.brightness)))
+        list.add(EditTypeItem(R.drawable.arrow_back_000000, getString(R.string.crop)))
+        list.add(EditTypeItem(R.drawable.arrow_back_000000, getString(R.string.rotation)))
+        resources.getStringArray(R.array.rotation_type).forEach {
+            list.add(EditTypeItem(R.drawable.arrow_back_000000, it))
+        }
+
+        return list
     }
 }
